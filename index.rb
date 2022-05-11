92bd08e1f3bc32018f6d52c52b5d27aa681306a8
iLynette <lynetteacholah@icloud.com>
2022-05-11 18:11:03 +0300

    A method to decode a morse code word

--- index.rb
+++ index.rb
@@ -10,3 +10,10 @@ def decode_char(morse_char)
   }
   morse_code_hash[morse_char]
 end
+
+def decode_word(morse_word)
+  characters = morse_word.split
+  decoded_character = ''
+  characters.each { |char| decoded_character += decode_char(char) }
+  decoded_character
+end
